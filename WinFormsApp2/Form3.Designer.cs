e4fcdced1579e0179e997bc76d8730af71a1e830
Borworn Kultumyotin <177917303+Byffify@users.noreply.github.com>
2025-11-04 18:06:04 +0700

    new commit

--- WinFormsApp2/Form3.Designer.cs
+++ WinFormsApp2/Form3.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form3";
+            SuspendLayout();
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Azure;
+            ClientSize = new Size(800, 561);
+            Name = "Form3";
+            Text = "Form3";
+            ResumeLayout(false);
         }
 
         #endregion
